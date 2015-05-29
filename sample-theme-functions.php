948940801189e54d2fa0deb74f305923be0b9a07
soderlind <per@soderlind.no>
2015-05-29 17:15:42 +0200

    set countries="all" to the shortcode

--- sample-theme-functions.php
+++ sample-theme-functions.php
@@ -7,7 +7,7 @@
 
 
 
-// Best practice, load parent style from functions.php don't use @import in style.css
+// Best practices, load parent style from functions.php don't use @import in style.css
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
@@ -21,7 +21,7 @@ require_once(dirname(__FILE__) . '/css-flags/css-flags.php');
 //add filter(s)
 add_action( 'init', function() {
 	add_filter('css-flags-countries', function() {
-		return array('no','se','dk'); // ISO_3166-1_alpha-2 codes: http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements
+		return array('all'); // ISO_3166-1_alpha-2 codes: http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements
 	});
 	// add_filter('css-flags-countries', function() {
 	// 	return array('all'); // ISO_3166-1_alpha-2 codes: http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements
@@ -66,16 +66,16 @@ class CSSFlagShortcode {
 		// Works only if the 'css-flags' has already been added
 		wp_add_inline_style( 'css-flags','
 			.box {
-				width: 32%;
-				/*float: left;*/
+				width: 15%;
+				float: left;
 			}
 			.flagbox {
-				width: 120px;
-				height: 120px;
+				width: 60px;
+				height: 60px;
 				display: inline-block;
 				vertical-align: middle;
 			}
-			.flag {
+			.css-flag {
 			    margin: auto;
 				display: inline-block;
 				vertical-align: middle;
@@ -91,14 +91,18 @@ class CSSFlagShortcode {
 	function cssflag( $atts) {
 
 		$atr = shortcode_atts( array(
-			'countries' => 'no',
-	    ), $atts);
+			'countries' => "all"
+	    ), $atts,'cssflag');
 		$countries = $this->multiexplode(array(",",".","|",":"),$atr['countries']);
+
+		if (in_array('all',$countries)) {
+			$countries = array('ad','ae','af','ag','ai','al','am','an','ao','ar','as','at','au','aw','ax','az','ba','basque','bb','bd','be','bf','bg','bh','bi','bj','bm','bn','bo','br','bs','bt','bv','bw','by','bz','ca','catalonia','cc','cd','cf','cg','ch','ci','ck','cl','cm','cn','co','cr','cs','cu','cv','cx','cy','cz','de','dj','dk','dm','do','dz','ec','ee','eg','eh','england','er','es','et','eu','fi','fj','fk','fm','fo','fr','ga','galicia','gb','gd','ge','gf','gg','gh','gi','gl','gm','gn','gp','gq','gr','gs','gt','gu','gw','gy','hk','hm','hn','hr','ht','hu','id','ie','il','im','in','io','iq','ir','is','it','je','jm','jo','jp','ke','kg','kh','ki','km','kn','kp','kr','kw','ky','kz','la','lb','lc','li','lk','lr','ls','lt','lu','lv','ly','ma','mc','md','me','mf','mg','mh','mk','ml','mm','mn','mo','mp','mq','mr','ms','mt','mu','mv','mw','mx','my','mz','na','nc','ne','nf','ng','ni','nl','no','np','nr','nu','nz','om','pa','pe','pf','pg','ph','pk','pl','pm','pn','pr','ps','pt','pw','py','qa','re','ro','rs','ru','rw','sa','sami','sb','sc','scotland','sd','se','sg','sh','si','sj','sk','sl','sm','sn','so','sr','st','sv','sy','sz','tc','td','tf','tg','th','tj','tk','tm','tm','tn','to','tr','tt','tv','tw','tz','ua','ug','um','us','uy','uz','va','vc','ve','vg','vi','vn','vu','wales','wf','ws','ye','yt','za','zm','zw');
+		}
 
 		$output = '';
 		if ( false !== wp_style_is( 'css-flags' ) ) {
 			foreach ( $countries as $country) {
-				$output .= sprintf('<div class="box"><div class="flagbox"><span class="flag cssflag %1$s %1$s-portrait"></span></div></div>', $country);
+				$output .= sprintf('<div class="box"><div class="flagbox"><span class="css-flag %1$s %1$s-portrait"></span></div></div>', $country);
 			}
 		} else {
 			$output = 'CSS Flags plugin/library is not added';
