28d6c9bac6c3ff70d738775d9f7d7d9dab6388f6
soderlind <per@soderlind.no>
2015-05-29 14:48:47 +0200

    added 'reset cache' and changed get_site_transient to get_transient

--- css.php
+++ css.php
@@ -14,18 +14,30 @@ $css_flags_start = $css_flags_time;
 $css_flags_cachetime = filter_var( apply_filters( 'css-flags-cachetime', 7200 ) , FILTER_VALIDATE_INT, array( 'default'    => 7200 ) );
 $css_flags_data_path = dirname( __FILE__ );
 
-if ( false === ( $css_flags = get_site_transient( "css-flags-all-countries" ) ) ) {
+//new version? reset the cache
+$css_flags_options = get_option( "CSS_Flags" );
+$css_flags_version = (isset($css_flags_options['version'])) ? $css_flags_options['version'] : '0';
+if ( $css_flags_version != CSSFLAGS_VERSION ) {
+	$css_flags_options['version'] = CSSFLAGS_VERSION;
+	delete_transient( 'css-flags-all-countries' );
+	delete_transient( 'css-flags-all-regions' );
+	update_option( "CSS_Flags", $css_flags_options );
+}
+
+//add country falgs to cache
+if ( false === ( $css_flags = get_transient( "css-flags-all-countries" ) ) ) {
 	$css_flags_json = file_get_contents( esc_url( $css_flags_data_path  . '/data/flags.json' ) );
 	$css_flags = json_decode( $css_flags_json, true );
 
-	set_site_transient( "css-flags-all-countries", $css_flags,  $css_flags_cachetime );
+	set_transient( "css-flags-all-countries", $css_flags,  $css_flags_cachetime );
 }
 
-if ( false === ( $css_flags_regions = get_site_transient( "css-flags-all-regions" ) ) ) {
+//add regions to cache
+if ( false === ( $css_flags_regions = get_transient( "css-flags-all-regions" ) ) ) {
 	$css_flags_json = file_get_contents( esc_url( $css_flags_data_path  . '/data/regions.json' ) );
 	$css_flags_regions = json_decode( $css_flags_json, true );
 
-	set_site_transient( "css-flags-all-regions", $css_flags_regions, $css_flags_cachetime );
+	set_transient( "css-flags-all-regions", $css_flags_regions, $css_flags_cachetime );
 }
 
 
