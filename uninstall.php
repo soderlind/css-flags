6da01273083bfc1604fb6a95d4d3db82ca3f82ac
soderlind <per@soderlind.no>
2016-07-28 10:37:45 +0200

    Update to WordPress Coding Standards

--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,7 @@
 <?php
 // If uninstall is not called from WordPress, exit
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    exit();
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
 }
 
 delete_transient( 'css-flags-all-countries' );
